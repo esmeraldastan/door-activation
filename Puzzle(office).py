f9dc834d6b94984a784af5a68ec6534f3a7496e8
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-03-29 11:47:41 -0700

    door
    door activation

--- Puzzle(office).py
+++ Puzzle(office).py
@@ -3,7 +3,7 @@ import sys
 
 print "Figure out the password in order to open the door."
 
-password = ["3546", "5515", "1651"] #passwords to open up the doors 
+password = "3546", "5515", "1651", "4539" #passwords to open up the doors 
 wordIndex = random.randint(0,len(password)-1)
 code_rip = password[wordIndex]
 user_guesses = ''
@@ -11,33 +11,32 @@ turns = 10 #the player will have only 10 times to try to figure out the code
 
 
 while turns > 0:
-        left = 0
-        for number in code_rip:
-            if number in user_guesses:
-                print number
-            else:
-                print "#"
-                left += 1
-            if left == 0:
-                print
-                print
-                print "Excellent, move on."# move into the next room 
-                break
+       left = 0
+       for number in code_rip:
+         if number in user_guesses:
+            print number,
+         else:
+            print "#",
+            left += 1
+       if left == 0:
+            print
+            print
+            print "Excellent, move on."# move into the next room 
+                
+            break
         
-      
+       print 
         
-            print 
-        
-            guess = raw_input("Guess the four Numbers:")
-            if guess in ['q','quit','exit']:
-                sys.exit(0)
-            user_guesses += guess 
-            if guess not in code_rip:
-                turns -=1
-                print "Sorry number not in the secret code. \n\n Please try again"
-                print "You have", +turns, 'left'
-            if turns == 0:
-                print "Sorry you lose"#change
+       guess = raw_input("Guess the four Numbers:")
+       if guess in ['q','quit','exit']:
+            sys.exit(0)
+       user_guesses += guess 
+       if guess not in code_rip:
+            turns -=1
+            print "Sorry number not in the secret code. \n\n Please try again"
+            print "You have", +turns, 'left'
+       if turns == 0:
+            print "Sorry you lose"#change
        
 
 
